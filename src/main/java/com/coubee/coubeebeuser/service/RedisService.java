9576688d7e68183cd3874539d9ae9b1c200d4881
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2025-08-19 14:10:47 +0900

    Fix: stg 환경 redis bean 생성 방지

--- src/main/java/com/coubee/coubeebeuser/service/RedisService.java
+++ src/main/java/com/coubee/coubeebeuser/service/RedisService.java
@@ -1,11 +1,13 @@
 package com.coubee.coubeebeuser.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Profile("local")
 public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
 
