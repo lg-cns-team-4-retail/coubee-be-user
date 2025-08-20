8b3a2b78bce25e331fad39ff40442ea185cb2fb1
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2025-08-20 16:44:50 +0900

    Fix: webconfig local에서만 사용하게끔 설정

--- src/main/java/com/coubee/coubeebeuser/config/WebConfig.java
+++ src/main/java/com/coubee/coubeebeuser/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.coubee.coubeebeuser.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("local")
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${file.upload.base-dir}")
